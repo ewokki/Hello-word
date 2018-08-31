58205946f338a317d85ba479a3fa776e9d9429dd
ewokki <42261874+ewokki@users.noreply.github.com>
2018-08-31 12:12:28 +0300

    Update CoinSpawner.cs

--- CoinSpawner.cs
+++ CoinSpawner.cs
@@ -8,9 +8,12 @@ public class CoinSpawner : MonoBehaviour {
 	int i;
 	
 	void SpawnCoins() {
+		//määrittää kentän koon
 		Vector3 size = GetComponent<Renderer>().bounds.size;
 		Vector3 center = GetComponent< Renderer>().bounds.center;
+		//määrittää mihin kolikko instantioituu
 		Vector3 position = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 1, Random.Range(-size.z / 2, size.z / 2));
+		//tarkastaa onko instantioudussa paikassa törmäystä muihin objekteihin
       var checkResult = Physics.OverlapSphere( position, 0.5F);
       if (checkResult.Length == 0 && Vector3.Distance(lastplace, position)> 20) {
         // all clear!
